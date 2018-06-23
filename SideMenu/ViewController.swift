c1c0894a403d54810a9aa992e48b76cf2cee6ea1
Sebastian Strus <sebastian.strus@opensolution.se>
2018-06-23 22:50:09 +0200

    Better timing in animations

--- SideMenu/ViewController.swift
+++ SideMenu/ViewController.swift
@@ -102,42 +102,50 @@ class ViewController: UIViewController {
         
         //move view
         UIView.animate(withDuration: 1.0, delay: 0.0, options: [], animations: {
-            self.shapeView.transform = CGAffineTransform(translationX: 138, y: 0)
+            //self.shapeView.transform = CGAffineTransform(translationX: 138, y: 0)
+            self.shapeView.frame = CGRect(x: self.shapeView.frame.origin.x + 138, y: self.shapeView.frame.origin.y, width: self.shapeView.frame.width, height: self.shapeView.frame.width)
         }, completion: nil)
         
         // profile pictur
         UIView.animate(withDuration: 0.5, delay: 1/7, options: [], animations: {
-            self.profileImageView.transform = CGAffineTransform(translationX: 69, y: 0)
+            //self.profileImageView.transform = CGAffineTransform(translationX: 69, y: 0)
+            self.profileImageView.frame = CGRect(x: self.profileImageView.frame.origin.x + 69, y: self.profileImageView.frame.origin.y, width: self.profileImageView.frame.width, height: self.profileImageView.frame.width)
         }, completion: nil)
         
         // icon1
         UIView.animate(withDuration: 0.5, delay: 1.3/7, options: [], animations: {
-            self.icon1View.transform = CGAffineTransform(translationX: 69, y: 0)
+            //self.icon1View.transform = CGAffineTransform(translationX: 69, y: 0)
+            self.icon1View.frame = CGRect(x: self.icon1View.frame.origin.x + 69, y: self.icon1View.frame.origin.y, width: self.icon1View.frame.width, height: self.icon1View.frame.width)
         }, completion: nil)
         
         // icon2
         UIView.animate(withDuration: 0.5, delay:1.5/7, options: [], animations: {
-            self.icon2View.transform = CGAffineTransform(translationX: 69, y: 0)
+            //self.icon2View.transform = CGAffineTransform(translationX: 69, y: 0)
+            self.icon2View.frame = CGRect(x: self.icon2View.frame.origin.x + 69, y: self.icon2View.frame.origin.y, width: self.icon2View.frame.width, height: self.icon2View.frame.width)
         }, completion: nil)
         
         // icon3
         UIView.animate(withDuration: 0.5, delay: 2/7, options: [], animations: {
-            self.icon3View.transform = CGAffineTransform(translationX: 69, y: 0)
+            //self.icon3View.transform = CGAffineTransform(translationX: 69, y: 0)
+            self.icon3View.frame = CGRect(x: self.icon3View.frame.origin.x + 69, y: self.icon3View.frame.origin.y, width: self.icon3View.frame.width, height: self.icon3View.frame.width)
         }, completion: nil)
         
         // icon4
         UIView.animate(withDuration: 0.5, delay: 2.5/7, options: [], animations: {
-            self.icon4View.transform = CGAffineTransform(translationX: 69, y: 0)
+            //self.icon4View.transform = CGAffineTransform(translationX: 69, y: 0)
+            self.icon4View.frame = CGRect(x: self.icon4View.frame.origin.x + 69, y: self.icon4View.frame.origin.y, width: self.icon4View.frame.width, height: self.icon4View.frame.width)
         }, completion: nil)
         
         // icon5
         UIView.animate(withDuration: 0.5, delay: 2.75/7, options: [], animations: {
-            self.icon5View.transform = CGAffineTransform(translationX: 69, y: 0)
+            //self.icon5View.transform = CGAffineTransform(translationX: 69, y: 0)
+            self.icon5View.frame = CGRect(x: self.icon5View.frame.origin.x + 69, y: self.icon5View.frame.origin.y, width: self.icon5View.frame.width, height: self.icon5View.frame.width)
         }, completion: nil)
         
         // icon6
         UIView.animate(withDuration: 0.5, delay: 3/7, options: [], animations: {
-            self.icon6View.transform = CGAffineTransform(translationX: 69, y: 0)
+            //self.icon6View.transform = CGAffineTransform(translationX: 69, y: 0)
+            self.icon6View.frame = CGRect(x: self.icon6View.frame.origin.x + 69, y: self.icon6View.frame.origin.y, width: self.icon6View.frame.width, height: self.icon6View.frame.width)
         }, completion: nil)
         
         
@@ -156,43 +164,51 @@ class ViewController: UIViewController {
         backgroundBtn.alpha = 0.0
         backgroundBtn.isUserInteractionEnabled = false
         UIView.animate(withDuration: 1.0) {
-            self.shapeView.transform = CGAffineTransform(translationX: -138, y: 0)
+            //self.shapeView.transform = CGAffineTransform(translationX: -138, y: 0)
+            self.shapeView.frame = CGRect(x: self.shapeView.frame.origin.x - 138, y: self.shapeView.frame.origin.y, width: self.shapeView.frame.width, height: self.shapeView.frame.width)
 
         }
         
         // profile pictur
-        UIView.animate(withDuration: 0.5, delay: 3.0/7, options: [], animations: {
-            self.profileImageView.transform = CGAffineTransform(translationX: -69, y: 0)
+        UIView.animate(withDuration: 0.5, delay: 2.5/7, options: [], animations: {
+            //self.profileImageView.transform = CGAffineTransform(translationX: -69, y: 0)
+            self.profileImageView.frame = CGRect(x: self.profileImageView.frame.origin.x - 69, y: self.profileImageView.frame.origin.y, width: self.profileImageView.frame.width, height: self.profileImageView.frame.width)
         }, completion: nil)
         
         // icon1
-        UIView.animate(withDuration: 0.5, delay: 2.75/7, options: [], animations: {
-            self.icon1View.transform = CGAffineTransform(translationX: -69, y: 0)
+        UIView.animate(withDuration: 0.5, delay: 2.4/7, options: [], animations: {
+            //self.icon1View.transform = CGAffineTransform(translationX: -69, y: 0)
+            self.icon1View.frame = CGRect(x: self.icon1View.frame.origin.x - 69, y: self.icon1View.frame.origin.y, width: self.icon1View.frame.width, height: self.icon1View.frame.width)
         }, completion: nil)
         
         // icon2
-        UIView.animate(withDuration: 0.5, delay: 2.5/7, options: [], animations: {
-            self.icon2View.transform = CGAffineTransform(translationX: -69, y: 0)
+        UIView.animate(withDuration: 0.5, delay: 2.3/7, options: [], animations: {
+            //self.icon2View.transform = CGAffineTransform(translationX: -69, y: 0)
+            self.icon2View.frame = CGRect(x: self.icon2View.frame.origin.x - 69, y: self.icon2View.frame.origin.y, width: self.icon2View.frame.width, height: self.icon2View.frame.width)
         }, completion: nil)
         
         // icon3
         UIView.animate(withDuration: 0.5, delay: 2/7, options: [], animations: {
-            self.icon3View.transform = CGAffineTransform(translationX: -69, y: 0)
+            //self.icon3View.transform = CGAffineTransform(translationX: -69, y: 0)
+            self.icon3View.frame = CGRect(x: self.icon3View.frame.origin.x - 69, y: self.icon3View.frame.origin.y, width: self.icon3View.frame.width, height: self.icon3View.frame.width)
         }, completion: nil)
         
         // icon4
         UIView.animate(withDuration: 0.5, delay: 1.5/7, options: [], animations: {
-            self.icon4View.transform = CGAffineTransform(translationX: -69, y: 0)
+            //self.icon4View.transform = CGAffineTransform(translationX: -69, y: 0)
+            self.icon4View.frame = CGRect(x: self.icon4View.frame.origin.x - 69, y: self.icon4View.frame.origin.y, width: self.icon4View.frame.width, height: self.icon4View.frame.width)
         }, completion: nil)
         
         // icon5
         UIView.animate(withDuration: 0.5, delay: 1/7, options: [], animations: {
-            self.icon5View.transform = CGAffineTransform(translationX: -69, y: 0)
+            //self.icon5View.transform = CGAffineTransform(translationX: -69, y: 0)
+            self.icon5View.frame = CGRect(x: self.icon5View.frame.origin.x - 69, y: self.icon5View.frame.origin.y, width: self.icon5View.frame.width, height: self.icon5View.frame.width)
         }, completion: nil)
         
         // icon6
-        UIView.animate(withDuration: 0.5, delay: 0, options: [], animations: {
-            self.icon6View.transform = CGAffineTransform(translationX: -69, y: 0)
+        UIView.animate(withDuration: 0.5, delay: 0.5/7, options: [], animations: {
+            //self.icon6View.transform = CGAffineTransform(translationX: -69, y: 0)
+            self.icon6View.frame = CGRect(x: self.icon6View.frame.origin.x - 69, y: self.icon6View.frame.origin.y, width: self.icon6View.frame.width, height: self.icon6View.frame.width)
         }, completion: nil)
         
         
